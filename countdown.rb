082d625b3a4fbd6c9e3bb668134552f55a1ab257
Carl Michael Oyam <carloyam21@icloud.com>
2020-06-21 05:23:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,7 @@
 def countdown(number = 12)
   while number > 0 
-  number -= 1
     puts "#{number} SECOND(S)!"
-    
+     number -= 1
       if number == 0 
   end      
-end
-
-def countdown(number)
-  while number > 0
-    puts "#{number} SECONDS(S)!"
-    number -= 1
-  end
- puts "HAPPY NEW YEAR!"
 end
